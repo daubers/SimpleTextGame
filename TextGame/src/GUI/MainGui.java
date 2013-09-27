ecce4f20e8221245f3d0c2372d58e43ecd38c573
Matthew Daubney <matt@daubers.co.uk>
2013-09-27 20:25:09 +0100

    First commit

---
+++ TextGame/src/GUI/MainGui.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Matt
+ * Date: 27/09/13
+ * Time: 20:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainGui {
+    private JTextArea outputTextArea;
+    private JTextField inputTextField;
+    private JButton btnDoIt;
+}

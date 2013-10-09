6f4747dba96cdf16fc5d2a63f3e3dc11af1628c9
Matthew Daubney <matt@daubers.co.uk>
2013-10-09 19:24:54 +0100

    Working on some tools and an XML structure to contain all of the level encounters/text etc

---
+++ src/Tools/CreateLevelXML.java
@@ -0,0 +1,11 @@
+package Tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Matt
+ * Date: 09/10/13
+ * Time: 19:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreateLevelXML {
+}
---
+++ src/Tools/GUI/LevelCreatorMain.java
@@ -0,0 +1,11 @@
+package Tools.GUI;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Matt
+ * Date: 09/10/13
+ * Time: 19:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class LevelCreatorMain {
+}
